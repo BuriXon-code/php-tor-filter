e717d5a3769fdef97cdcd53c5cb48781ae7b9b20
Kamil BuriXon Burek <burixon.developement@gmail.com>
2025-07-10 12:58:02 +0200

    SET 'PASS' COOKIE

---
+++ pass.php
@@ -0,0 +1,15 @@
+<?php
+//##############################################
+//# Author: Kamil BuriXon Burek (BuriXon-code) #
+//# Name: php-tor-filter (c) 2025              #
+//# Description: A PHP script for filtering    #
+//#              clients using the Tor network #
+//#              ( Script allowing Tor IP      #
+//#               addresses through. )         #
+//# Version: v 1.0                             #
+//# Changelog: release                         #
+//# Todo:                                      #
+//##############################################
+	// if everything allright, create pass-cookie
+	setcookie('tor_passed', '1', 0, '/');
+?>
